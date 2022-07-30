3046f5ab9254da69e17d8316dbf5a54a3efcb963
sg0947214 <rafal.szczepankiewicz.ctr@sabre.com>
2022-07-31 00:17:27 +0200

    + use block for fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,9 +7,9 @@ const wrapPageElement = ({ element }) => (
     <Helmet htmlAttributes={{ lang: 'en' }}>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-      <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Freehand&display=swap&text=RS" />
+      <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Freehand&display=block&text=RS" />
       <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" />
-      <link href="https://fonts.googleapis.com/css2?family=Freehand&display=swap&text=RS" rel="stylesheet" media="print" onLoad="this.media='all'" />
+      <link href="https://fonts.googleapis.com/css2?family=Freehand&display=block&text=RS" rel="stylesheet" media="print" onLoad="this.media='all'" />
       <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" media="print" onLoad="this.media='all'" />
     </Helmet>
     {element}
